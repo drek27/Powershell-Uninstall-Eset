a6c9b6f0325cd25cf0877499da19c6e969498293
drek27 <37086791+drek27@users.noreply.github.com>
2021-06-02 13:06:39 +0200

    Script Powershell French
    Script version français permettant de supprimer Eset endpoint et son agent

---
+++ SupprésionEset.ps1
@@ -0,0 +1,51 @@
+Set-ExecutionPolicy Bypass
+
+Write-Host `
+" _____________________________________
+|                                     |
+|      Script de Suppréssion ESET     |
+|      Endpoint Security + Agnet      |
+|                                     |
+| (c) Drek_27                         |
+|                                     |
+ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯" `
+-ForegroundColor Yellow
+
+Write-Host "Assurez-vous de bien lancer le script depuis le Poste au quelle vous avez besoin de déinstaller un logiciel " -ForegroundColor Red
+Read-Host "Entré le mot de passe pour la déinstallation de ESET Endpoint Security"
+
+$start = Read-host "Vous les vous continuer ? [O] / [N]"
+if ( $start -eq 'O' ) {
+
+$namepc = Get-ComputerInfo CsCaption | Select-Object CsCaption 
+
+$uninstall64 = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "ESET Endpoint Security" } | select UninstallString
+
+if ($uninstall64) {
+$uninstall64 = $uninstall64.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+$uninstall64 = $uninstall64.Trim()
+Write "Supprésion..."
+start-process "msiexec.exe" -arg "/X $uninstall64 /norestart password=$password" -Wait}
+
+
+$uninstall64 = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "ESET Management Agent" } | select UninstallString
+
+if ($uninstall64) {
+$uninstall64 = $uninstall64.UninstallString -Replace "msiexec.exe","" -Replace "/I","" -Replace "/X",""
+$uninstall64 = $uninstall64.Trim()
+Write "Supprésion..."
+start-process "msiexec.exe" -arg "/X $uninstall64 /norestart" -Wait}
+
+Set-NetFirewallProfile -Profile * -Enabled True
+
+Write-Host "Processus de vérification de la suppréssion ..."
+$namelistappli = Get-WmiObject -Class Win32_Product | Select-Object -Property Name
+
+if ($namelistappli -eq "Eset Management Agent" -and "ESET Endpoint Security" ){
+    Write-Host "Eset Endpoint n'est pas supprimer" -BackgroundColor Black -ForegroundColor Green
+}
+else{
+    Restart-Computer -ComputerName $namepc
+}
+}
+Else{Write-Host "Bye !"}
